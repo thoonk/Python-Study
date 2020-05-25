45bf70b343d9cef0eca3d632cfde2f18d7f138f5
dkrmrm878 <dkrmrm878@gmail.com>
2020-05-25 22:16:03 +0900

    Fix input to tri

--- Baekjoon/Math/Q4153_rightTriangle.py
+++ Baekjoon/Math/Q4153_rightTriangle.py
@@ -1,9 +1,9 @@
 while True:
-    input = list(map(int, input().split()))
-    m = max(input)
-    if sum(input)==0: break
-    input.remove(m)
-    if input[0]**2 + input[1]**2 == m**2:
+    tri = list(map(int, input().split()))
+    m = max(tri)
+    if sum(tri)==0: break
+    tri.remove(m)
+    if tri[0]**2 + tri[1]**2 == m**2:
         print('right')
     else:
         print('wrong')
